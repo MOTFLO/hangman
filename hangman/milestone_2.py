363c0e49c5e6ef2cda0d6b959bafd4ae6b018e82
MOTFLO <motco_florin2008@yahoo.com>
2022-10-27 17:11:20 +0100

    Chooses random words from the list

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,5 +1,8 @@
-favorite_fruits = ["apples", "banana", "strawberry", "raspberry", "orange"]
+import random
 
+favorite_fruits = ["apples", "banana", "strawberry", "raspberry", "orange"]
 word_list = favorite_fruits
 
-print (word_list)
+random.choice (word_list)
+word = random.choice(word_list)
+print (word)
