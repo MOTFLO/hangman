74f27125f8b844222dbe23fad1bbb91244000013
MOTFLO <motco_florin2008@yahoo.com>
2022-11-01 16:05:26 +0000

    Checks if input is a single character

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -10,3 +10,8 @@ word = random.choice(word_list)
 print (word)
 
 guess = input("Please enter a single letter:")
+if len(guess) <= 1 and guess.isalpha () == True:
+        print ("Good guess!")
+else:
+    print ("Oops! That is not a valid input.")
+        
