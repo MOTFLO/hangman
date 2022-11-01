251cb321c30fd96e47a537820e19a4fbecf63a91
MOTFLO <motco_florin2008@yahoo.com>
2022-11-01 15:21:08 +0000

    Takes input from user

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,5 +1,6 @@
 import random
 
+
 favorite_fruits = ["apple", "banana", "orange", "pear", "strawberry"]
 word_list = favorite_fruits
 print (word_list)
@@ -7,3 +8,5 @@ print (word_list)
 random.choice(word_list)
 word = random.choice(word_list)
 print (word)
+
+guess = input("Please enter a single letter:")
