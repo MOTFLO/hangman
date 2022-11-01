f48d791fe76be8afdc9feaef7e54bd24c36ef9cb
MOTFLO <motco_florin2008@yahoo.com>
2022-11-01 13:29:34 +0000

    File milestone 2 deleted

--- hangman/milestone_2.py
+++
@@ -1,8 +0,0 @@
-import random
-
-favorite_fruits = ["apples", "banana", "strawberry", "raspberry", "orange"]
-word_list = favorite_fruits
-
-random.choice (word_list)
-word = random.choice(word_list)
-print (word)
