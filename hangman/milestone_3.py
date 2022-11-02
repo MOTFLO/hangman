13583f9ea6c412fafce2fca9bc3510e2282a7a6d
MOTFLO <motco_florin2008@yahoo.com>
2022-11-02 16:11:12 +0000

    Checking whether the guess is in the word

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,10 +1,19 @@
+import random
 from ast import Break
 
 guess = input("Please guess a letter:")
+word_list = ["apple", "banana", "orange", "pear", "strawberry"]
+
+
 while len(guess) <= 1 and guess.isalpha () == True:
-    print ("Good guess!")
+    random.choice(word_list)
+    word = random.choice(word_list)
+    if guess in word:
+        print ("Good guess!" + guess + " is in the " + word + ".")
+    else:
+        print ("Sorry, " + guess + " is not in the word. Try again.")
     guess = input("Please guess a letter:")
     Break
 else:
     print ("Invalid letter. Please, enter a single alphabetical character.")
-    
+ 
