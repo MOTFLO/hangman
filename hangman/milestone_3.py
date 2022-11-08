3eb6a021c825e87dc412e897cb3ce80cf66a4098
MOTFLO <motco_florin2008@yahoo.com>
2022-11-08 14:20:08 +0000

    Functions to run the checks

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,19 +1,30 @@
 import random
 from ast import Break
 
-guess = input("Please guess a letter:")
 word_list = ["apple", "banana", "orange", "pear", "strawberry"]
 
-
-while len(guess) <= 1 and guess.isalpha () == True:
-    random.choice(word_list)
-    word = random.choice(word_list)
-    if guess in word:
-        print ("Good guess!" + guess + " is in the " + word + ".")
+def check_guess(guess):
+    guess = guess.lower()
+    while len(guess) <= 1 and guess.isalpha() == True:
+        random.choice(word_list)
+        word = random.choice(word_list)
+        if guess in word:
+            print ("Good guess!" + guess + " is in the " + word + ".")
+        else:
+            print ("Sorry, " + guess + " is not in the word. Try again.")
+        guess = input("Please guess a letter:")
+        guess = guess.lower()
+        Break
     else:
-        print ("Sorry, " + guess + " is not in the word. Try again.")
+        return check_guess
+
+
+def ask_for_input():
     guess = input("Please guess a letter:")
-    Break
-else:
-    print ("Invalid letter. Please, enter a single alphabetical character.")
- 
+    if check_guess(guess) == True and guess in check_guess.word(guess):
+        print (check_guess)
+    else:
+        print ("Invalid letter. Please, enter a single alphabetical character.")
+        return ask_for_input
+
+ask_for_input()   
