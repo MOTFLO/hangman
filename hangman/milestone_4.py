4f3120c369422359bf665e6a31c20a6e5d441a1c
MOTFLO <motco_florin2008@yahoo.com>
2022-11-08 23:38:02 +0000

    Methods for running the checks

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -1,6 +1,7 @@
 import random
 
-class Hangman:
+class Hangman():
+    
     word_list = ["apple", "banana", "orange", "pear", "strawberry"]
     num_lives = 5
     
@@ -8,7 +9,32 @@ class Hangman:
     word = random.choice(word_list)
     
     word_guessed = ['_', '_', '_', '_', '_']
-    num_letters = int('')
+    num_letters = int()
     list_of_guesses = []
     
-_init_ = Hangman 
+    def check_guess(guess, word):
+        gues = guess.lower()
+        if guess in word:
+            print ("Good guess! " + guess + " is in the word.")
+        return
+    
+        
+    
+    def ask_for_input(list_of_guesses, check_guess, word):
+        while True:
+            guess = input("Please, enter a letter:")
+            guess = guess.lower()
+            if  len(guess) > 1 or guess.isalpha() == False:
+                 print ("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in list_of_guesses:
+                print ("You already tried that letter!")
+            else:
+                if len(guess) <= 1 and guess.isalpha() == True:
+                    check_guess(guess, word)
+                    list_of_guesses.append(guess)
+                    print (list_of_guesses)
+        return
+               
+    ask_for_input(list_of_guesses, check_guess, word)
+        
+_init_ = Hangman()
