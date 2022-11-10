5d9180e2677b000c125ee00baadda9dabe1ebf29
MOTFLO <motco_florin2008@yahoo.com>
2022-11-10 11:51:36 +0000

    Deffining a letter already in word

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -8,19 +8,26 @@ class Hangman():
     random.choice(word_list)
     word = random.choice(word_list)
     
-    word_guessed = ['_', '_', '_', '_', '_']
+    word_guessed = []
     num_letters = int()
     list_of_guesses = []
     
-    def check_guess(guess, word):
+    def check_guess(guess, word, word_guessed, num_letters):
         gues = guess.lower()
         if guess in word:
             print ("Good guess! " + guess + " is in the word.")
+            for i in range(0, len(word)):
+                if word[i] == guess:
+                    i = i + 1
+                    word_guessed = str(i) + " : " + guess
+                    print (word_guessed)
+            num_letters -= 1
+            
         return
     
         
     
-    def ask_for_input(list_of_guesses, check_guess, word):
+    def ask_for_input(list_of_guesses, check_guess, word, word_guessed, num_letters):
         while True:
             guess = input("Please, enter a letter:")
             guess = guess.lower()
@@ -30,11 +37,11 @@ class Hangman():
                 print ("You already tried that letter!")
             else:
                 if len(guess) <= 1 and guess.isalpha() == True:
-                    check_guess(guess, word)
+                    check_guess(guess, word, word_guessed, num_letters)
                     list_of_guesses.append(guess)
                     print (list_of_guesses)
         return
                
-    ask_for_input(list_of_guesses, check_guess, word)
+    ask_for_input (list_of_guesses, check_guess, word, word_guessed, num_letters)
         
 _init_ = Hangman()
