3288be0c4bf18ff2bbbed1164dd769f00d7c93c9
MOTFLO <motco_florin2008@yahoo.com>
2022-11-15 23:26:06 +0000

    Changes in code milestone 3 and 4

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -5,29 +5,35 @@ class Hangman:
     word_list = ["apple", "banana", "orange", "pear", "strawberry"]
     list_of_guesses = []
     word = random.choice(word_list)
-    word_guessed = ['_', '_', '_', '_', '_']
+    word_guessed = ['_']
     num_letters = int()
     num_lives = 5
     
     def _init_(self, word_list, num_lives):
         self.word_list = word_list
         self.num_lives = num_lives
-        
-    def _init_(self, word):
-        self.word = word
-
-    def _init_(self, word_guessed):
-        self.word_guessed = word_guessed
-    
-    def _init_(self, num_letters):
-        self.num_letters = num_letters
-       
-    def _init_(self, list_of_guesses):
-        self.list_of_guesses = list_of_guesses
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_']
+        self.num_letters = int()
+        self.list_of_guesses = []
             
     def check_guess(self, guess):
+        #guess = input("Please, guess a letter:")
         if guess in self.word:
             print("Good guess! " + guess + " is in the word.")
+            for i in range(0, len(self.word)):
+                if self.word[i] == guess:
+                    self.word_guessed= str(self.word_guessed)[:i] + guess + str(self.word_guessed)[i + 1:]
+                    print (self.word_guessed)
+            self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print ("Sorry, " + guess + " is not in the word.")
+            print ("You have " + str(self.num_lives) + " lives left.")
+            self.list_of_guesses.append(guess)
+            print (self.list_of_guesses)
+        return
+    
     
     def ask_for_input(self):
         while True:
@@ -41,7 +47,6 @@ class Hangman:
                 if len(guess) <= 1 and guess.isalpha() == True:
                     self.check_guess(guess)
                     self.list_of_guesses.append(guess)
-                    print (self.list_of_guesses)
         return
               
 game = Hangman()
